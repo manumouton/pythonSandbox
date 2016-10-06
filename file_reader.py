0f752315461107b0f56587786fef257051e52874
Manu Mouton <manumouton@gmail.com>
2016-10-06 13:48:10 +0200

    checking profanity in text service; playing with rest api and webservices

--- file_reader.py
+++ file_reader.py
@@ -1,6 +1,26 @@
+import urllib.request
+import urllib.parse
+import urllib.error
+
+
 def read_text(path):
     quotes = open(path)
     content = quotes.read()
     print(content)
+    check_profanity(content)
+    quotes.close()
+
+
+def check_profanity(text_to_check):
+    url = 'http://www.purgomalum.com/service/containsprofanity?text=' + text_to_check
+    print(url)
+    req = urllib.request.Request(url)
+    try:
+        response = urllib.request.urlopen(req)
+        output = response.read()
+        print("output: " + output.decode())
+    except urllib.error.URLError as e:
+        print(e.reason)
+
 
 read_text("./quotes.txt")
