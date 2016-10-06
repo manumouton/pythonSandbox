2b367ce11b6718f61a054700147417c49758bc98
Manu Mouton <manumouton@gmail.com>
2016-10-06 15:30:16 +0200

    Playing with inheritance, movie site, ...

--- inheritance.py
+++ inheritance.py
@@ -1,9 +1,9 @@
-class Parent():
+class Parent:
     def __init__(self, name=None, eyes_color=None):
         self.name = name
         self.eyes_color = eyes_color
 
 class Child(Parent):
-    def __init__(self, name=None, eyes_color=None, size):
+    def __init__(self, name=None, eyes_color=None, size=None):
         Parent.__init__(name, eyes_color)
         self.size = size
